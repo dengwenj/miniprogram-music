4db8845613026a6e6d929eee090dee92af81f4cb
dengwenj <927561237@qq.com>
2022-05-15 18:40:29 +0800

    区域头部组件

--- miniprogram/pages/music/index.ts
+++ miniprogram/pages/music/index.ts
@@ -17,7 +17,12 @@ Page({
 
   getPageData() {
     getBanners().then((res) => {
+      // setData 在设置 data 数据上是同步的
+      // 通过最新的数据对 wxml 进行渲染，渲染的过程时异步的， 两个数据不一样
       this.setData({ banners: res.banners })
+      // console.log(this.data.banners); 获取到的最新数据
+
+      // react 里面 this.setState 是异步的（不包含定时器，promise 对象等等），渲染的过程也是异步的
     })
   },
 
